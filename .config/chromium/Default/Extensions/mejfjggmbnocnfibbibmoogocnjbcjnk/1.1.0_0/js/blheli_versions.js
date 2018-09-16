3f3b1ed8f7e7fc18d7969eed666d3b5c41e139ba
StillANixRookie <gvales2831997@gmail.com>
2018-09-16 00:50:47 +0000

    Sun 16 Sep 2018 00:50:47 GMT

--- .config/chromium/Default/Extensions/mejfjggmbnocnfibbibmoogocnjbcjnk/1.1.0_0/js/blheli_versions.js
+++
@@ -1,3 +0,0 @@
-var BLHELI_VERSIONS_REMOTE = 'https://raw.githubusercontent.com/blheli-configurator/blheli-configurator/master/js/blheli_versions.json';
-var BLHELI_VERSIONS_LOCAL = './js/blheli_versions.json';
-var BLHELI_VERSIONS_KEY = 'versions';
